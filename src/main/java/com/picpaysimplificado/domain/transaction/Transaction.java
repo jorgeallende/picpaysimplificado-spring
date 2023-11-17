1c2cee10fd1ff461e29b46fde5f6651a0d747381
jorgeallende <jorgeallende333@gmail.com>
2023-11-16 22:34:40 -0300

    feat: create UserRepository

---
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,30 @@
+package com.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+
+}
